38eee50749e118a67aba8d3343d61ba64ae16afc
rishabhpuri2006 <rishabh0620.becse24@chitkara.edu.in>
2025-03-29 17:26:13 +0530

    Solution to Count the Number of Prime Numbers in an Array

--- Count the Number of Prime Numbers in an Array@@676c02002dbc8c3e7a280b7d/code.c
+++ Count the Number of Prime Numbers in an Array@@676c02002dbc8c3e7a280b7d/code.c
@@ -1,30 +1,20 @@
-#include <stdio.h>
-#include <math.h>  // Required for sqrt()
-
-int isComposite(int num) {
-    if (num < 2) return 0;  // Ignore numbers < 2
-    for (int i = 2; i <= sqrt(num); i++) {
-        if (num % i == 0) return 1;  // Found a divisor, it's composite
+#include<stdio.h>
+int main(){
+int x;
+scanf("%d",&x);
+int arr[x];
+for (int i=0;i<=x-1;i++){
+    scanf("%d",&arr[i]);
     }
-    return 0; // If no divisor found, it's prime
-}
-
-int main() {
-    int x;
-    scanf("%d", &x);
-    
-    int arr[x];
-    for (int i = 0; i < x; i++) {
-        scanf("%d", &arr[i]);
+int count=0;
+for (int i=0;i<=x-1;i++){
+    for(int j=4;j<=100;j++){
+    if(arr[i]%j==0){
+    printf("%d",arr[i]);
+    count++;
+    break;
     }
-
-    int count = 0;
-    for (int i = 0; i < x; i++) {
-        if (isComposite(arr[i])) {
-            printf("%d ", arr[i]);
-            count++;
-        }
     }
-
+    }
     return 0;
 }
